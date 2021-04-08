e8876e3c26a1eb90a2bd141d77878014233dc190
Vulka9n <you@example.com>
2021-04-08 20:07:40 +0530

    Added Header

---
+++ src/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header() {
+  return (
+    <Container>
+      <TitleContainer>To Do List</TitleContainer>
+    </Container>
+  );
+}
+
+export default Header;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 50px;
+  background-color: #ffb037;
+  box-shadow: 0 5px 20px grey;
+`;
+
+const TitleContainer = styled.h1``;
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+function Home() {
+  return <Container>I am home</Container>;
+}
+
+export default Home;
+
+const Container = styled.div``;
